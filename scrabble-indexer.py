5685db281521efc3329ef92cbe735ba1c075beb5
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-06-11 13:23:10 -0400

    Fixes bugs and cleans up code

--- scrabble-indexer.py
+++ scrabble-indexer.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python
+
 import cPickle
 import sys
-from scrabble import ngram,sorted_word_list
+import os
+from scrabble import ngram,sorted_word_list,gram_file
+from functools import partial
+
+import pdb
+
 
 class GramIndexer(object):
-	def __init__(self,source,index_name='gramindex.txt',maxn=4):
+	def __init__(self,source,index_folder='grams/',maxn=4):
 		self._dictionary = sorted_word_list(source)
 		self._words = {num:word for num,word in self.ranked_words}
 		self._maxn = maxn
-		self._index_name = index_name
+		self._index_folder = index_folder
+		self._gram_file = partial(gram_file,folder=self._index_folder)
+
 
 	@property
 	def ranked_words(self):
@@ -15,36 +24,31 @@ class GramIndexer(object):
 		return enumerate(self._dictionary) # ranked words
 
 	def supergram(self,word):
-		"""Generates all n-grams of the word, in the range n=1..self.maxn
-
-		At each iteration, it yields a tuple: (n,n-grams(word))
+		"""Generates lists of all n-grams of the word, in the range n=1..self.maxn
 		"""
 		max_length = min(len(word),self._maxn)
 		for i in range(1,max_length+1):
-			yield i,ngram(word,n=i)
+			yield ngram(word,n=i)
 
-	def index_grams(self,word_rank,n,grams):
+
+	def index_grams(self,word_rank,grams):
 		"""
-		Adds each set of n-grams for a word to the index assigned to 
-		grams of size n
+		Adds each word to all the files that contain its gram
 
 		* uses the word's rank instead string as the value
-		Ex: Word: ''
 
 		"""
-		index = self.index[n]
-
 		for gram in grams:
-			entry = index.setdefault(gram,[])
-			entry.append(word_rank)
+			with open(self._gram_file(gram),'a') as f:
+				f.write(str(word_rank)+'\n')
 
 	def generate_index(self):
 		"""Indexes all n-grams of size 1..maxn that appear in the Scrabble dictionary,
 		storing the ranking of each word that contains that n-gram
 
-		Ex: self.index[4]=> index of 2-grams
+		Ex: if maxn==4:
 			To find all matches for 'ourd': 
-			g.index[4]['ourd'] => [69474, 80707, 88030, 93060, 96907, 100629, 100630, 106365]
+			open('grams/ourd.txt') => [69474, 80707, 88030, 93060, 96907, 100629, 100630, 106365]
 			
 			Where:
 				69474 => 'bourdons'
@@ -52,23 +56,17 @@ class GramIndexer(object):
 				88030 => 'sourdines'
 			    93060 => 'gourdes', etc.
 			                                  """
-		self.index = {n:{} for n in range(self._maxn+1)}
+		if not os.path.exists(index_folder):
+			os.makedirs(index_folder)
+			
 		for position,word in self.ranked_words:
-			for n,grams in self.supergram(word):
-				self.index_grams(position,n,grams)
-
-	def export_index(self):
-		"""Pickles a copy of the index and word key and writes it to disk"""
-		with open(self._index_name,'w') as f:
-			cPickle.dump(self._words,f,2)
-			cPickle.dump(self.index,f,2)
-		print "Index has been written to {}".format(self._index_name)
+			for grams in self.supergram(word): # make so all words indexed before writing to a file
+				self.index_grams(position,grams)
 
 if __name__ == '__main__':
 	if len(sys.argv)==2:
 		word_source = sys.argv[1]
 		g = GramIndexer(word_source)
 		g.generate_index()
-		g.export_index()
 	else:
 		print "Correct Usage: scrabble-indexer word-list"
--- scrabble-suggester.py
+++ scrabble-suggester.py
@@ -1,27 +1,34 @@
+#!/usr/bin/env python
+
 import itertools
 import heapq
-from scrabble import ngram,ismatch
+from scrabble import ngram,ismatch,gram_file
 import cPickle
 from functools import partial
 import sys
-
+import importlib
+import os
+import pdb
+si = __import__('scrabble-indexer')
 
 class GramSuggester(object):
-	def __init__(self,index_name='gramindex.txt',maxn=4):
-		self.maxn = maxn
-		with open(index_name,'r') as f:
-			self.words = cPickle.load(f)
-			self.index = cPickle.load(f)
+	def __init__(self,index_folder='grams',maxn=4):
+		self._maxn = maxn
+		self._index_folder = index_folder
+		self._gram_file = partial(gram_file,self._index_folder)
+		self._open_files = [] # list of all files opened for a given query
+		self._words = si.GramIndexer('dictionary.txt')._words
+
 
 	def maxgram(self,Q):
 		"""Returns a tuple of the maximum size n-grams of Q and that maxn value
 
 		Ex: if self.maxn = 4 (i.e. the index only indexes up to 4-grams):
 			maxgram('octopus') => (set(['octo', 'topu', 'ctop', 'opus']), 4)
-			maxgram('hat') =>  (set[('hat')], 3)                 """
-
-		maxsize = min(len(Q),self.maxn)
-		return ngram(Q,maxsize),maxsize
+			maxgram('hat') =>  (set[('hat')], 3)
+			                 """
+		maxsize = min(len(Q),self._maxn)
+		return ngram(Q,maxsize) if maxsize > 0 else []
 
 	def word(self,word_rank):
 		"""Returns the word corresponding to an absolute word_rank
@@ -29,45 +36,57 @@ class GramSuggester(object):
 		Ex: word(0) =>  photosynthesizing, 
 		which means that photosynthesizing has the highest scrabble score
 		in the dictionary                                             """
-		return self.words[word_rank]
+		return self._words[word_rank]
 
-	def values(self,gram):
-		"""Generator that yields ranks of words matching gram, in 
-		descending order of scrabble score (and ascending rank)
+	def matches(self,gram):
+		"""Returns an open file that corresponds to matches to gram"""
+		fn = self._gram_file(gram)
+		if not os.path.exists(fn):
+			return []
 
-		Ex: values('octo') => [33883, 42033, 42956, 42957, 48766, 52321, 52322, 52323, 
-		59771, 63517, 63518, 64582, 71175, 74865, 74866, 75892, 82157, 85543, 85544, 91969]
+		open_file = open(fn)
+		self._open_files.append(open_file)
+		return (int(word.strip('\n')) for word in open_file)
 
-		"""
-		index = self.index[len(gram)]
+	def _merge(self,*its):
+		"""Merges the sorted iterators in *its, returning an empty list if any of them is empty"""
+		if [] in its:
+			return []
+		return heapq.merge(*its)
 
-		return (value for value in index[gram])
-	
+	def _intersect(self,*its):
+		"""Returns a generator that yields all values that appear in all *its """
+		intersection = (value for value,instances in itertools.groupby(self._merge(*its)) if len(list(instances))==len(its))
+
+		return intersection
 
 	def intersect(self,*grams):
 	    """Returns a generator that yields all values matching all n-grams in grams """
-	    gram_matches = [self.values(gram) for gram in grams]
-
-	    if len(gram_matches)==1:
-	    	return gram_matches.pop()
-
-		return (value for value,instances in itertools.groupby(heapq.merge(*gram_matches)) 
-			if len(list(instances))==len(gram_matches))
-
-	def top_results(self,Q,K):
-		grams,n = self.maxgram(Q)
+	    if len(grams)==1:
+	    	return self.matches(grams[0])
+	    intersection = self._intersect(*map(self.matches,grams))
+	    return intersection
+		
+	def gram_matches(self,Q):
+		"""Returns a lazy iterator over all words that contain matches to all ngrams
+		in the query string"""
+		gram_s = itertools.imap(self.word,self.intersect(*self.maxgram(Q)))
+		return gram_s
+	
+	def exact_matches(self,Q):
+		"""Returns a lazy iterator over all words that match the query string
+		exactly"""
 		matches_query = partial(ismatch,Q)
+		return itertools.ifilter(matches_query,self.gram_matches(Q))
+	
+	def top_results(self,Q,K):
+		"""Returns an iterator over the top K words that match the
+		query string"""
+		results = list(itertools.islice(self.exact_matches(Q),K))
+		for f in self._open_files:
+			f.close()
+		return results
 
-		try:
-			#All words that contain all the n-grams of the query string
-			possible_matches = itertools.imap(self.word,self.intersect(*grams)) 
-
-			#All words that exactly match the query string
-			exact_matches = itertools.ifilter(matches_query,possible_matches)
-
-			return list(itertools.islice(exact_matches,K))
-		except KeyError:
-			return []
 
 if __name__ == '__main__':
 	if len(sys.argv)==3:
