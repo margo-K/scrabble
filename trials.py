637b4d530863fdb8856da3b02b208df67f4b7010
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-06-11 00:12:53 -0400

    Removes separate logs for K values

--- trials.py
+++ trials.py
@@ -1,7 +1,7 @@
 
 from pprint import pprint
 from queries import generate_query_strings
-# from brutesuggester import BruteSuggester,BruteSortedSuggester
+from brutesuggester import BruteSortedSuggester
 import pdb
 import math
 from timeit import Timer
@@ -21,44 +21,41 @@ class LogItem(object):
 	def __init__(self,setup_str,fn_str,name):
 		self.setup_str = setup_str
 		self.fn_str = fn_str
-		self.log = {k:[] for k in [5,10,100,1000]}
-		# self.k = K
+		self.log = []
 		self.name = name
 
 	def time(self,q):
 		for k in [5,10,100,1000]:
 			timer = Timer(self.fn_str,self.setup_str.format(q,k))
-			self.log[k].append(min(timer.repeat(3,number=10)))
+			self.log.append(min(timer.repeat(3,number=10)))
 
-	def stats(self,k):
-		data = self.log[k]
+	def stats(self):
+		data = self.log
 		return avg(data),stdev(data),min(data),max(data)
 
 	def report(self):
-		print '{:^20}{:^20}{:^20}{:^20}{:^20}'.format('K','Mean','Stdev','Min','Max')
-		print '{:-^100}'.format('')
-		for k in sorted(list(self.log.keys())):
-			print '{:^20}{:^20}{:^20}{:^20}{:^20}'.format(k,*self.stats(k))
+		# pdb.set_trace()
+		return '{:^20}{:^20}{:^20}{:^20}{:^20}'.format(self.name,*self.stats())
 
 
 if __name__=='__main__':
-  
-    queries = generate_query_strings(size=4,query_number=100)
-    print "Done generating queries"
     grams_setup="ss = __import__('scrabble-suggester');gs=ss.GramSuggester();Q='{}';K={}"
     grams_fn = 'gs.top_results(Q,K)'
 
-    # sorted_setup="from brutesuggester import BruteSortedSuggester;ss=BruteSortedSuggester('dictionary.txt');Q='{}';K={}"
-    # sorted_fn = 'ss.top_results(Q,K)'
+    sorted_setup="from brutesuggester import BruteSortedSuggester;ss=BruteSortedSuggester('dictionary.txt');Q='{}';K={}"
+    sorted_fn = 'ss.top_results(Q,K)'
+    for i in xrange(1,7):
+    	queries = generate_query_strings(size=2,query_number=10)
+    	print "Done generating queries"
+    	print '{:^20}{:^20}{:^20}{:^20}{:^20}'.format('Suggester','Mean','Stdev','Min','Max')
+    	print '{:-^100}'.format('')
+    	g = LogItem(grams_setup,grams_fn,name='Scrabble Suggester')
+    	s = LogItem(sorted_setup,sorted_fn,name='Brute Sorted')
+    	for q in queries:
+	    	g.time(q)
+	    	s.time(q)
+	 	print g.report()
+	 	print s.report()
 
-    g = LogItem(grams_setup,grams_fn,name='Scrabble Suggester')
-    # s = LogItem(sorted_setup,sorted_fn,name='brutesorted')
 
-    for q in queries:
-    	g.time(q)
-    g.report()
-
-    	# s.time(q)
-    	# s.report()
-    	
 
