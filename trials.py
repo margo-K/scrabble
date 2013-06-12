4efe36bc741ebfcfcc6be86baa741305e70c104f
Margo Kulkarni <margo.r.kulkarni@gmail.com>
2013-06-12 00:28:06 -0400

    Cleans up and yields working graphing functionality

--- trials.py
+++ trials.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 from pprint import pprint
-from queries import generate_query_strings
+from queries import queries
 import pdb
 import math
 from timeit import Timer
 import sys
+import matplotlib.pyplot as plt
+grams_setup="ss = __import__('scrabble-suggester');name='suggester';Q='{}';K={};printing=False"
+grams_fn = 'ss.main([name,Q,K],printing)'
+
 
 def avg(s):
 	return sum(s)/float(len(s))
@@ -16,79 +20,100 @@ def stdev(s):
 def variance(s):
 	return avg(map(lambda x: (x - avg(s))**2, s))
 
-class Logger(object):
-    def __init__(self,setup_str,fn_str,k_vals=[5,10,50,100,500,1000],n_vals=range(1,17)):
-        self.setup_str = setup_str
-        self.fn_str = fn_str
-        self.logk = {k:[] for k in k_vals}
-        self.logn = {n:[] for n in n_vals}
-        self.k_vals = k_vals
-        self.n_vals = n_vals
+def time(n,k,number=10):
+    log = []
+    for q,k in queries(size=n,K=k,number=1):
+        timer = Timer(grams_fn,grams_setup.format(q,k))
+        time = min(timer.repeat(3,number=number))/number
+        log.append(time)
+    return log
 
-    def timek(self,q,number=10):
-            for k in self.k_vals:
-                timer = Timer(self.fn_str,self.setup_str.format(q,k))
-                time = min(timer.repeat(3,number=number))/number
-                self.logk[k].append(time)
+def report(log,name,variables):
+    print '{:^10}|{:^20}|{:^20}|{:^20}|'.format(name,'Mean','StdDev','Max')
+    print '{:-^10}|{:-^20}|{:-^20}|{:-^20}'.format('','','','')
+    for val in variables:
+        print '{:^10}|{:^20}|{:^20}|{:^20}'.format(val,*self.stats(log))
 
-    def timen(self,k,number=10):
-        for n in self.n_vals:
-            queries = generate_query_strings(n,query_number=100)
-            for q in queries:
-                timer = Timer(self.fn_str,self.setup_str.format(q,k))
-                time = min(timer.repeat(3,number=number))/number
-                self.logn[n].append(time)
+def stats(values):
+    return avg(values),stdev(values),max(values)
 
-    def stats(self,val,log):
-        data = log[val]
-        return avg(data),stdev(data),max(data)
-
-    def report(self,log,constant,variables):
-        print '{:^10}|{:^20}|{:^20}|{:^20}|'.format(constant,'Mean','StdDev','Max')
-        print '{:-^10}|{:-^20}|{:-^20}|{:-^20}'.format('','','','')
-        for val in variables:
-    		print '{:^10}|{:^20}|{:^20}|{:^20}'.format(val,*self.stats(val,log))
+def get_time(n,k):
+    log = time(n,k)
+    return avg(log)
 
 
+def plot_Q(qsizes):
+    """Plots a graph of Q length vs. time
+    K-vals can be at most 4"""
+    print "I at least got into the fn Q"
+    x_data = qsizes
+    k_vals = [10,50,100,500,1000]
+    y_colors = (color for color in ["blue","red","green","yellow","purple"])
 
-if __name__=='__main__':
-    grams_setup="ss = __import__('scrabble-suggester');name='suggester';Q='{}';K={};printing=False"
-    grams_fn = 'ss.main([name,Q,K],printing)'
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1) # one row, one column, first plot
+    # Plot the data.
 
-    for n in [1,2,10,15,20]:
-            g = Logger(grams_setup,grams_fn,n_vals=n)
-            queries = generate_query_strings(n,query_number=100)
-            for query in queries:
-                g.timek(query)
-            print "N:{}".format(n)
-            g.report(g.logk,'K',g.)
+    for k in k_vals:
+        color = y_colors.next()
+        y_vals = ys(qsizes,k)
+        ax.scatter(x_data,y_vals,color=color,label="K:{}".format(k)) #color="blue", linestyle="dashed", linewidth="3")
 
-    for query in queries:
-        g.timek(query)
-        g.report(g.logk,'K',g._kvals)
+    # Add a title.
+    ax.set_title("Average Query Times by Query Length")
+    # Add some axis labels.
+    ax.set_xlabel("Q-Length")
+    ax.set_ylabel("Time (s)")
+    plt.legend(loc=2)
 
-    for k in [5,10,50,100,500,1000]:
-        print k
-        g.timen(k)
-        g.report(g.logn,'N',g.n_vals)
+    # Produce an image.
+    fig.show()
+    fig.savefig("qplotallQ.png")
 
-import csv
-import datetime as dt
-import matplotlib.pyplot as plt
+def plot_K(k_vals):
+    """Plots a graph of K vs. time"""
+    print "I at least got into the fn K"
 
-x,y = [],[]
-csv_reader = csv.reader(open('data.csv'))
-for line in csv_reader:
-    x.append(int(line[0]))
-    y.append(dt.datetime.strptime(line[1],'%M:%S.%f'))
+    x_data = k_vals
+    q_sizes = [1,2,5,10,20]
+    y_colors = (color for color in ["blue","red","green","yellow","purple"])
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(y,x,'o-')
-fig.autofmt_xdate()
+    all_y = []
 
-    
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1) # one row, one column, first plot
 
+    for q in q_sizes:
+        color = y_colors.next()
+        y_vals = kys(q,k_vals)
+        ax.scatter(x_data,y_vals,color=color,label="Q:{}".format(q)) #color="blue", linestyle="dashed", linewidth="3")
+
+    # Add a title.
+    ax.set_title("Average Query Times By K-Values")
+    # Add some axis labels.
+
+    ax.set_xlabel("K")
+    ax.set_ylabel("Time (s)")
+    plt.legend(loc=2)
+    # Produce an image.
+    fig.show()
+    fig.savefig("qplotallK.png")
+
+def kys(qsize,kvals):
+    y_data = []
+    for k in kvals:
+        y_data.append(get_time(qsize,k))
+    return y_data
+
+def ys(qsizes,k):
+    y_data = []
+    for n in qsizes:
+        y_data.append(get_time(n,k))
+    return y_data
+
+if __name__ == '__main__':
+    plot_Q(range(1,10))
+    plot_K(range(1,15))
 
 
    
